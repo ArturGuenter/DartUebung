a4d41b38bbb1a820d23517c472e8d0b1c9f68698
ArturGuenter <artur.guenter@icloud.com>
2025-10-13 22:56:34 +0200

    - Liste verbinden

--- .idea/DatenStrukturen.dart
+++ .idea/DatenStrukturen.dart
@@ -1,6 +1,7 @@
 void main(){
 
   List x = [1, true , 2 ,3.5, "vier"];
+  List y = ["rtu", 99, 4.2];
       print(x[1]);
       x[0] = 2;
   print(x[0]);
@@ -9,4 +10,6 @@ void main(){
   print(x[4]);
   x.removeAt(4);
   print(x[4]);
+  List z = x+y ;
+  print(z);
 }
