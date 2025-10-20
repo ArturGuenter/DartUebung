90adfd4d78a0f7d715ca255fe58bce56270e3f78
ArturGuenter <artur.guenter@icloud.com>
2025-10-20 20:42:53 +0200

    - Bedinungen if else beispiel note

--- .idea/Bedinungen.dart
+++ .idea/Bedinungen.dart
@@ -1,10 +1,16 @@
 void main(){
 
 String wetter = "Sonnig";
+
+int note = 70;
+
     if(wetter == "Sonnig") {
       print("es ist sonnig");
     } else {
       print("Mediathek");
     }
 
+    if(note >=90) {
+      print("sehr gut")
+    }
 }
