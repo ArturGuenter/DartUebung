ddde3da678cb7e9176e1c50d843d01e976b5c5b1
ArturGuenter <artur.guenter@icloud.com>
2025-10-22 20:25:17 +0200

    - schleifen for in in verbindung mit if

--- .idea/Schleifen.dart
+++ .idea/Schleifen.dart
@@ -1,12 +1,17 @@
+
 void main(){
 
   List<dynamic> z = ["Tier", 2, 3.14, "Haus"];
 
-  for(int i= 0; i<5 ; i++){
-    print(i);
-  }
+  //for(int i= 0; i<5 ; i++){
+  //  print(i);
+  //}
 
   for(var i in z ){
-    print(i);
+    if(i.runtimeType == int ){
+      print(i);
+    }
+
+
   }
 }
