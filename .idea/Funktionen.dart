af1f9fd9ffd6d95cc364b78f97961ff2e47690d7
ArturGuenter <artur.guenter@icloud.com>
2025-10-25 19:07:46 +0200

    - init funktionen backeKuchen mit while und Ausgabe der Funktion

--- .idea/Funktionen.dart
+++ .idea/Funktionen.dart
@@ -1,4 +1,4 @@
-String backeKuchen(String1 zutat1, String zutat2, int anzahlKuchen){
+String backeKuchen(String zutat1, String zutat2, int anzahlKuchen){
 int gebackeneKuchen = 0;
 String kuchenBeschreibung= "";
 
@@ -6,10 +6,12 @@ while(gebackeneKuchen< anzahlKuchen){
   gebackeneKuchen++;
 }
   kuchenBeschreibung = "${anzahlKuchen} Kuchen mit ${zutat1} und ${zutat2}";
-return kuchenBeschreibung
+return kuchenBeschreibung;
 }
 
 void main(){
+String schokoladenKuchen = backeKuchen("Kakao", "Mehl", 3);
+print(schokoladenKuchen);
 
 
 }
