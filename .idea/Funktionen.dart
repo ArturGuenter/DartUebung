acc651464f62b3b74a0ba6c6cb3457c1ab287ea4
ArturGuenter <artur.guenter@icloud.com>
2025-10-25 19:03:54 +0200

    - init funktionen backeKuchen mit while

--- .idea/Funktionen.dart
+++ .idea/Funktionen.dart
@@ -2,6 +2,10 @@ String backeKuchen(String1 zutat1, String zutat2, int anzahlKuchen){
 int gebackeneKuchen = 0;
 String kuchenBeschreibung= "";
 
+while(gebackeneKuchen< anzahlKuchen){
+  gebackeneKuchen++;
+}
+
 }
 
 void main(){
