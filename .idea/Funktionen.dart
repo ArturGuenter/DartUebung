64659bd3496d84297de23d689a40aa036fbb999b
ArturGuenter <artur.guenter@icloud.com>
2025-10-26 21:58:25 +0100

    - init funktionen backeKuchen mit while und Ausgabe der Funktion ... ohne rückgabewert

--- .idea/Funktionen.dart
+++ .idea/Funktionen.dart
@@ -1,17 +1,8 @@
-int backeKuchen(String zutat1, String zutat2, int anzahlKuchen){
-int gebackeneKuchen = 0;
-String kuchenBeschreibung= "";
-
-while(gebackeneKuchen< anzahlKuchen){
-  gebackeneKuchen++;
-}
-  kuchenBeschreibung = "${anzahlKuchen} Kuchen mit ${zutat1} und ${zutat2}";
-return 20;
+void printMessage(String message){
+  print(message);
 }
 
 void main(){
-int schokoladenKuchen = backeKuchen("Kakao", "Mehl", 3);
-print(schokoladenKuchen);
-
+printMessage("hallo");
 
 }
