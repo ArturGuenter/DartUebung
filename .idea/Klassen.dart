81aeb76784a520fdbd30aab6285ce17652500e90
ArturGuenter <artur.guenter@icloud.com>
2025-10-27 21:50:00 +0100

    - klassen main konstruktor

--- .idea/Klassen.dart
+++ .idea/Klassen.dart
@@ -1,3 +1,20 @@
+class Donut{
+  final String flavor;
+  final double price;
+  final String size;
+  final String imageName;
+
+  Donut({
+    required this.flavor,
+    required this.price,
+    required this.size,
+    required this.imageName
+}
+      );
+
+
+}
+
 void main(){
 
 
